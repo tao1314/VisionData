2233ad4f4a297b032369f742edbf859c3868cbba
tao1314 <daring962464@163.com>
2026-09-24 11:38:24 +0800

    更新滚动条样式

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -31,6 +31,7 @@ declare module 'vue' {
     ElRadioButton: typeof import('element-plus/es')['ElRadioButton']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
     ElSelect: typeof import('element-plus/es')['ElSelect']
+    ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTooltip: typeof import('element-plus/es')['ElTooltip']
